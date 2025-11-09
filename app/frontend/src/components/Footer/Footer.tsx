c42146fe14506b97d886c5b948f29a921809030a
William Bo <william.zhi.bo@gmail.com>
2025-11-09 02:20:38 -0500

    add emots and update footer disclaimers

--- app/frontend/src/components/Footer/Footer.tsx
+++ app/frontend/src/components/Footer/Footer.tsx
@@ -1,25 +1,15 @@
 import styles from './Footer.module.css';
-import RegionSelector from '../RegionSelector';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <div className={styles.left}>
-          <span className={styles.copyright}>
-            © 2025 Rift Rewind. All rights reserved.
-          </span>
-        </div>
-        
-        <div className={styles.center}>
-          <RegionSelector />
-        </div>
-        
-        <div className={styles.right}>
-          <span className={styles.disclaimer}>
-            Rift Rewind isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties.
-          </span>
-        </div>
+        <p className={styles.copyright}>
+          © 2025 Rift Rewind. All rights reserved.
+        </p>
+        <p className={styles.disclaimer}>
+          Rift Rewind isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.
+        </p>
       </div>
     </footer>
   );
